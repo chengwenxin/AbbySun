9794d0c3313eaa5230e5e50f7a9631755d1653ec
ChengWenxin <1260077156@qq.com>
2018-10-01 16:24:18 +0800

    npm run eject

--- abby/src/index.js
+++ abby/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Login from './components/login'
 import Register from './components/register'
-import {Browser,Route,Switch} from 'react-router-dom'
+import {BrowserRouter,Route} from 'react-router-dom'
 ReactDOM.render(
-    <Browser>
+    <BrowserRouter>
         <Route path="/login" component={Login}></Route>
         <Route path="/register" component={Register}></Route>
-    </Browser>
+    </BrowserRouter>
     
     , document.getElementById('root'));
 registerServiceWorker();
